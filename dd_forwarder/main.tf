e4d0141cf2306b5624844a31517c2244ad169a39
ttsknst <tkinoshita29@gmail.com>
2021-03-02 15:37:13 +0900

    dd fix

--- dd_forwarder/main.tf
+++ dd_forwarder/main.tf
@@ -1,11 +1,12 @@
 module "datadog_forwarder" {
   source = "./modules/datadog_stack"
 
-  api_key   = "fizzbuzzfizzbuzz"
-  role      = "dev"
-  s3_bucket = "buzz.dev.bucket"
-
-  filter_pattern = "php"
+  api_key        = "fizzbuzzfizzbuzz"
+  role           = "dev"
+  s3_bucket      = "fizz.dev.com"
+  dd_version     = "3.28.4"
+  logging_bucket = "buzz.dev.com"
+  log_prefix     = "fizz"
 
   bucket_prefix = [
     "fizz/",
--- dd_forwarder/versions.tf
+++ dd_forwarder/versions.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 3.18.0"
     }
+    template = {
+      source  = "hashicorp/template"
+      version = "~> 2.1.2"
+    }
   }
   required_version = ">= 0.13"
 }
